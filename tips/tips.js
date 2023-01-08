95a033d070874669fdd58f0cf82afb18f16a3872
Gonzalo <gonzalomartinvg@gmail.com>
2023-01-08 18:41:39 -0300

    filtrado continente pagina tip

--- tips/tips.js
+++ tips/tips.js
@@ -1,90 +1,216 @@
+// 1) DATOS DE LOS CONTINENTES
+
 const america = {
     
     "brasil":[
         {
-            "nombre": "Río de Janeiro (BRA)",
+            "nombre": "Brasil",
 
-            "url": "rio-de-janeiro.html",
+            "url": "",
 
-            "tipoDestinoPrimario": "playa",
+            "imagenBandera": "images/brasil-bandera-02.png",
             
-            "tipoDestinoSecundario": "montana",
+            "imagenDestino": "images/brasil-02.jpg",
         }
     ],
 
-    "ilha-grande":[
+    "mexico":[
         {
-            "nombre": "Ilha Grande (BRA)",
+            "nombre": "México",
 
-            "url": "ilha-grande.html",
+            "url": "",
 
-            "tipoDestinoPrimario": "montana",
+            "imagenBandera": "images/mexico-bandera-02.png",
             
-            "tipoDestinoSecundario": "playa",
-
+            "imagenDestino": "images/mexico-02.jpg",
         }
     ],
 
-    "parati":[
+    "colombia":[
         {
-            "nombre": "Parati (BRA)",
+            "nombre": "Colombia",
 
-            "url": "parati.html",
+            "url": "",
+
+            "imagenBandera": "images/colombia-bandera-02.png",
+            
+            "imagenDestino": "images/colombia-02.jpg",
         }
     ],
 
-    "ilhabela-sao-sebastiao":[
+    "argentina":[
         {
-            "nombre": "Ilhabela y São Sebastião (BRA)",
+            "nombre": "Argentina",
 
-            "url": "ilhabela-sao-sebastiao.html",
+            "url": "",
 
+            "imagenBandera": "images/argentina-bandera-02.png",
+            
+            "imagenDestino": "images/argentina-02.jpg",
         }
     ],
 
-    "ubatuba":[
+    "uruguay":[
         {
-            "nombre": "Ubatuba (BRA)",
+            "nombre": "Uruguay",
 
-            "url": "ubatuba.html",
+            "url": "",
 
+            "imagenBandera": "images/uruguay-bandera-02.png",
+            
+            "imagenDestino": "images/uruguay-02.jpg",
         }
     ],
 
-    "maresias-ilha-das-couves":[
+    "panama":[
         {
-            "nombre": "Maresías e Ilha Das Couves (BRA)",
+            "nombre": "Panamá",
 
-            "url": "maresias-ilha-das-couves.html",
+            "url": "",
 
+            "imagenBandera": "images/panama-bandera-02.png",
+            
+            "imagenDestino": "images/panama-02.jpg",
         }
     ],
-
-    "arraial-do-cabo":[
-        {
-            "nombre": "Arraial do Cabo (BRA)",
-
-            "url": "arraial-do-cabo.html",
-
-        }
-    ],
-
-    "todos":[
-        {
-            "nombre": "Otros destinos",
-
-            "url": "https://es.wikipedia.org/wiki/Paraty",
-        }
-    ]
-
-    ,
-
-    "america":[
-        {
-            "nombre": "Destinos en América",
-
-            "url": "https://es.wikipedia.org/wiki/Paraty",
-        }
-    ]
 
 }
+
+const asia = {
+    
+    "maldivas":[
+        {
+            "nombre": "Maldivas",
+
+            "url": "",
+
+            "imagenBandera": "images/maldivas-bandera-02.png",
+            
+            "imagenDestino": "images/maldivas-02.jpg",
+        }
+    ],
+
+    "tailandia":[
+        {
+            "nombre": "Tailandia",
+
+            "url": "",
+
+            "imagenBandera": "images/tailandia-bandera-02.png",
+            
+            "imagenDestino": "images/tailandia-02.jpg",
+        }
+    ],
+
+    "malasia":[
+        {
+            "nombre": "Malasia",
+
+            "url": "",
+
+            "imagenBandera": "images/malasia-bandera-02.png",
+            
+            "imagenDestino": "images/malasia-02.jpg",
+        }
+    ],
+
+
+}
+
+// 2) FUNCIÓN QUE MUESTRA LOS PAÍSES (Esta función se ejecuta para que aparezca la información al iniciar la página. Luego, la función "clicnEnBotonContinente" permite el filtrado por continente).
+
+function mostrarPaisesDelContinenteElegido(a){
+
+    //Función que muestra los países del continente seleccionado
+
+    let imagenBandera = "";
+    let imagenDestino = "";
+    let urlPaginaPais = "";
+    let nombrePais = "";
+
+    let bloquerAutogenerador = document.querySelector(".bloque-autogenerador");
+
+    for (prop in a){
+
+        let imagenBandera = (a[prop][0].imagenBandera);
+        let imagenDestino = (a[prop][0].imagenDestino);
+        let urlPaginaPais = (a[prop][0].url);
+        let nombrePais = (a[prop][0].nombre);
+
+        const elementoAutoGenerado = document.createElement("a");
+
+        const contenidoAutoGenerado = //Las `` siguientes se utilizan para hacer un html literal
+                `
+                <a href="${urlPaginaPais}" class="contenedor-autogenerado" style="background-image: url(${imagenDestino})">
+      
+                    <img class="bandera" src=${imagenBandera} alt="Bandera país">
+                    <p class="nombre-pais">${nombrePais}</p>
+                    <span class="barra-pais"></span>
+
+                </a>
+
+                `; 
+
+        elementoAutoGenerado.innerHTML = contenidoAutoGenerado;
+
+        bloquerAutogenerador.append(elementoAutoGenerado);
+
+            }
+
+ }
+
+ //Ejecutamos la función con todos los continentes (los comentados son porque todavía no los visité)
+
+ mostrarPaisesDelContinenteElegido(america);
+ mostrarPaisesDelContinenteElegido(asia)
+//  mostrarPaisesDelContinenteElegido(europa);
+//  mostrarPaisesDelContinenteElegido(africa)
+//  mostrarPaisesDelContinenteElegido(oceania);
+
+
+// 3) FUNCIÓN QUE PERMITE FILTRAR POR CONTINENTE (A la función le pasamos el parámetro "a" y luego la ejecutamos con todos los continentes. Esto es para que sea reutilizable la función). Los botones permiten el filtrado, es decir, muestra solamente los que corresponden al continente elegido.
+
+function clickEnBotonContinente (a) {
+
+    let continenteElegido = document.querySelector(a);
+
+    continenteElegido.addEventListener("click", function() {
+
+        let bloquerAutogenerador = document.querySelector(".bloque-autogenerador"); //Seleccionas el bloque donde se genera el HTML de los países.
+        bloquerAutogenerador.innerHTML=""; //Limpiamos la información preexistente del bloque. (Esto es para que no se repita información).
+
+        //Los dataset.nombrecontinente están en el HTML
+
+        if (continenteElegido.dataset.nombrecontinente == "america"){
+            mostrarPaisesDelContinenteElegido(america)
+        }
+
+        if (continenteElegido.dataset.nombrecontinente == "europa"){
+            mostrarPaisesDelContinenteElegido(europa)
+        }
+
+        if (continenteElegido.dataset.nombrecontinente == "asia"){
+            mostrarPaisesDelContinenteElegido(asia)
+        }
+
+        //Los continentes de abajo todavía no los visité, para activarlos, hay que generar un objeto con la info igual que los otros continentes y a su vez, hay que habilitar para que se vean los botones mediante el css "styles-tips.css" cambiando el display:none de las clases "africa" y "oceania".
+
+        if (continenteElegido.dataset.nombrecontinente == "africa"){
+            mostrarPaisesDelContinenteElegido(europa)
+        }
+
+        if (continenteElegido.dataset.nombrecontinente == "oceania"){
+            mostrarPaisesDelContinenteElegido(asia)
+        }
+
+    });
+
+}
+
+//Ejecutamos la función con todos los continentes
+
+clickEnBotonContinente(".america")
+clickEnBotonContinente(".asia")
+clickEnBotonContinente(".europa")
+clickEnBotonContinente(".africa")
+clickEnBotonContinente(".oceania")
